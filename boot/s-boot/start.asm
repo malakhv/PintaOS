618ced0562362d15951f798c1b4601920fbcb4b7
Mikhail Malakhov <malakhv@gmail.com>
2022-05-13 14:04:30 +0300

    Fix commtnts

--- boot/s-boot/start.asm
+++ boot/s-boot/start.asm
@@ -1,11 +1,11 @@
-#
-# Copyright (C) 2022 Mikhail Malakhov <malakhv@gmail.com>
-# 
-# This file is a part of S-Boot project.
-#
-# Confidential and Proprietary. All Rights Reserved.
-# Unauthorized copying of this file, via any medium is strictly prohibited.
-#
+//
+// Copyright (C) 2022 Mikhail Malakhov <malakhv@gmail.com>
+//
+// This file is a part of S-Boot project.
+//
+// Confidential and Proprietary. All Rights Reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+//
 
 .code16                 # Using 16-bit code
 
@@ -17,12 +17,12 @@
 //
 _start:
 
-    # Put your code here
+    // Put your code here
 
 _mbr:
     # Added MBR signature
     . = _start + 510     # Move to 510 byte from 0
-    .byte 0x55           # Added MBR signature (byte 1)
-    .byte 0xaa           # Added MBR signature (byte 2)
+    .byte 0x55
+    .byte 0xaa
 
-# END
+// END
